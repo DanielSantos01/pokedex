ed850413c7bdff3c1f063d3a751ab03ac0e94b4f
DanielSantos01 <danielspovv@gmail.com>
2022-01-13 10:36:24 -0300

    feat: react-native setup

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{backgroundColor: '#ffff', height: '100%'}}>
+      <Text>Running pokedex</Text>
+    </View>
+  );
+};
+
+export default App;
